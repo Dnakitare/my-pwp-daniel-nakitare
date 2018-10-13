09c27c3fe8ef5c6d8fbbf7eb946c65f20d65cf8d
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-13 17:29:26 -0600

    beginnings

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"
+	</head>
+	<body>
+		<h2>Purpose</h2>
+		<p>To provide potential employers with my information and show case my portfolio.</p>
+		<h2>Audience</h2>
+		<p>professional peers and potential employers</p>
+		<h2>Goal</h2>
+		<p>To acquire full-time employment or an internship.</p>
+	</body>
+</html>
