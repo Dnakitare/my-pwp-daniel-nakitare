d65fe70d228bd9cc9d2653f1e4efb10866190224
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-13 17:45:45 -0600

    Outlined

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,5 +11,13 @@
 		<p>professional peers and potential employers</p>
 		<h2>Goal</h2>
 		<p>To acquire full-time employment or an internship.</p>
+		<h1>Persona</h1>
+		<p><strong>Name:</strong></p>
+		<p><strong>Age:</strong></p>
+		<p><strong>Role:</strong></p>
+		<p><strong>Hobbies:</strong></p>
+		<p><strong>Technology Used:</strong></p>
+		<p><strong>Likes:</strong></p>
+		<p><strong>Dislikes:</strong></p>
 	</body>
 </html>
