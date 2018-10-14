d7a750dab70ac5a663374ca179fd99e3493033ad
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-13 18:03:59 -0600

    Persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,21 +16,19 @@
 		<p><strong>Age:</strong>43</p>
 		<p><strong>Role:</strong> Hiring manager at Foo Dev Shop</p>
 		<p><strong>Hobbies:</strong> Cycling, Hiking, FPV Drone Racing</p>
-		<p><strong>Technology Used:</strong>
+		<p><strong>Technology Used:</strong></p>
 			<ul>
 				<li>Hades Canyon NUC i7-8809G Windows 10 Home</li>
 				<li>13" Macbook Pro 2017 OS X 10.14</li>
 				<li>Google Pixel 3</li>
 				<li>Airpods</li>
 		 </ul>
-		</p>
-		<p><strong>Likes:</strong>
+		<p><strong>Likes:</strong></p>
 			<ul>
 				<li>Clear, concise, communication</li>
 				<li>Simple, efficient, design</li>
 				<li>Idea meritocracy</li>
 			</ul>
-		</p>
 		<p><strong>Dislikes:</strong></p>
 	</body>
 </html>
