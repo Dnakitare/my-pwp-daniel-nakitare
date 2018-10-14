ca0e41f01e78acce89642a74179118241f10b501
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-13 18:02:33 -0600

    Persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,16 +8,29 @@
 		<h2>Purpose</h2>
 		<p>To provide potential employers with my information and show case my portfolio.</p>
 		<h2>Audience</h2>
-		<p>professional peers and potential employers</p>
+		<p>Professional peers and potential employers</p>
 		<h2>Goal</h2>
 		<p>To acquire full-time employment or an internship.</p>
 		<h1>Persona</h1>
-		<p><strong>Name:</strong></p>
-		<p><strong>Age:</strong></p>
-		<p><strong>Role:</strong></p>
-		<p><strong>Hobbies:</strong></p>
-		<p><strong>Technology Used:</strong></p>
-		<p><strong>Likes:</strong></p>
+		<p><strong>Name:</strong>John Doe</p>
+		<p><strong>Age:</strong>43</p>
+		<p><strong>Role:</strong> Hiring manager at Foo Dev Shop</p>
+		<p><strong>Hobbies:</strong> Cycling, Hiking, FPV Drone Racing</p>
+		<p><strong>Technology Used:</strong>
+			<ul>
+				<li>Hades Canyon NUC i7-8809G Windows 10 Home</li>
+				<li>13" Macbook Pro 2017 OS X 10.14</li>
+				<li>Google Pixel 3</li>
+				<li>Airpods</li>
+		 </ul>
+		</p>
+		<p><strong>Likes:</strong>
+			<ul>
+				<li>Clear, concise, communication</li>
+				<li>Simple, efficient, design</li>
+				<li>Idea meritocracy</li>
+			</ul>
+		</p>
 		<p><strong>Dislikes:</strong></p>
 	</body>
 </html>
