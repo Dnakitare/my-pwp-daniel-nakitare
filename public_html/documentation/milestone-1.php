3941b4fcf37ccab8f22d36d1d7a000f250436ccd
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-16 15:58:28 -0600

    Edited structure

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 	<head>
 		<meta charset="UTF-8">
 		<title>Daniel Nakitare's PWP: Milestone 1</title>
 	</head>
+
 	<body>
 		<h2>Purpose</h2>
 		<p>To provide potential employers with my information and show case my portfolio.</p>
@@ -18,34 +20,36 @@
 		<p><strong>Role:</strong> Hiring manager at Foo Dev Shop</p>
 		<p><strong>Hobbies:</strong> Cycling, Hiking, FPV Drone Racing, Eve Online</p>
 		<p><strong>Technology Used:</strong></p>
+		<ul>
+			<li>Hades Canyon NUC i7-8809G Windows 10 Home</li>
 			<ul>
-				<li>Hades Canyon NUC i7-8809G Windows 10 Home</li>
-				<ul>
-					<li>Asus PG348Q Monitor</li>
-					<li>Razer Naga Trinity Mouse</li>
-					<li>Razer Huntsman Keyboard</li>
-				</ul>
-				<li>13" Macbook Pro 2017 OS X 10.14</li>
-				<li>Google Pixel 3</li>
-				<li>Airpods</li>
-		 </ul>
+				<li>Asus PG348Q Monitor</li>
+				<li>Razer Naga Trinity Mouse</li>
+				<li>Razer Huntsman Keyboard</li>
+			</ul>
+			<li>13" Macbook Pro 2017 OS X 10.14</li>
+			<li>Google Pixel 3</li>
+			<li>Airpods</li>
+		</ul>
 		<p><strong>Likes:</strong></p>
-			<ul>
-				<li>Clear, concise, communication</li>
-				<li>Simple, efficient, design</li>
-				<li>Idea meritocracy</li>
-			</ul>
+		<ul>
+			<li>Clear, concise, communication</li>
+			<li>Simple, efficient, design</li>
+			<li>Idea meritocracy</li>
+		</ul>
 		<p><strong>Dislikes:</strong></p>
-			<ul>
-				<li>Scrolling through personal web pages to find professional network links</li>
-				<li>Bland, impersonal, candidate sites</li>
-				<li>Bad design choices</li>
-			</ul>
+		<ul>
+			<li>Scrolling through personal web pages to find professional network links</li>
+			<li>Bland, impersonal, candidate sites</li>
+			<li>Bad design choices</li>
+		</ul>
 		<p><strong>Motivation:</strong></p>
 		<p>Looking for a full-stack developer to replace a departing team member on a critical project that is already behind schedule.</p>
 		<p><strong>User Case:</strong></p>
-		<p>John is searching for the right candidate to fill a newly empty spot on the development team of his small start-up. The previous candidate was a solid full-stack developer but lacked design sense when it came to frontend development. John has been looking at the personal web pages of entry-level developers in Albuquerque after hearing that the Deep Dive Bootcamp has recently graduated a new cohort. He recently met on of the graduates at a tech mixer and wants to find out if the new dev has not only the technical skill but design sense that he needs to complete the current project and hopefully stay on to add value to the company in the long term.</p>
+		<p>John is searching for the right candidate to fill a newly empty spot on the development team of his small start-up. The previous candidate was a solid full-stack developer but lacked design sense when it came to frontend development. John has
+			been looking at the personal web pages of entry-level developers in Albuquerque after hearing that the Deep Dive Bootcamp has recently graduated a new cohort. He recently met on of the graduates at a tech mixer and wants to find out if the new dev
+			has not only the technical skill but design sense that he needs to complete the current project and hopefully stay on to add value to the company in the long term.</p>
 		<p><strong>User Goal:</strong></p>
-		<p>Determine whether Daniel Nakitare possesses the technical and interpersonal skills required and the design sense desired.</p>
+		<p>Determine whether Daniel Nakitare possesses the technical and interpersonal skills required for the position and the design sense desired.</p>
 	</body>
 </html>
