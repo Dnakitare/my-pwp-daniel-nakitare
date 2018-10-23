00da9cb7ba5ab486a393653ddd1187f5e8cfe1a0
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-23 13:54:12 -0600

    milestone-2 documentation creation

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -4,17 +4,14 @@
 		<meta charset="UTF-8">
 		<title>Daniel Nakitare's PWP: Milestone 2a</title>
 	</head>
-
 	<body>
 		<h2>Welcome:</h2>
 		<p>I am Daniel Nakitare and I am a Full Stack Developer. This message will be overlaid on a large picture of the Sandia Mountains.</p>
 		<h2>About Me:</h2>
-		<p>This section with detail my technical skill set and professional competencies. I will highlight my strengths and personality. My core skills and programming languages will be prominently displayed in this section and the design will focus on making it easy to read quickly accessible from floating navigation bar.</p>
+		<p>This section with detail my technical skill set and professional competencies. I will highlight my strengths and personality. My core skills and programming languages will be prominently displayed in this section and the design will focus on making it easy to read quickly accessible from floating navigation bar via smooth scroll.</p>
 		<h2>Portfolio:</h2>
 		<p>I want this to be a responsive carousel containing 3-5 examples of my best work. Pop hovers will supply more information about my contribution to each project and the technology used.</p>
 		<h2>Contact:</h2>
 		<p>This section will contain a simple contact form and my professional email address. My professional social media links will live in the floating navigation bar.</p>
 	</body>
-
-
 </html>
