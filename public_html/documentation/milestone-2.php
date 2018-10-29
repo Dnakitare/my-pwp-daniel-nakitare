5ce2bc6b100611f52374f0d2c358834225c69ac2
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-28 19:01:57 -0600

    milestone 2a - text edits

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,11 +8,11 @@
 		<h2>Welcome:</h2>
 		<p>Daniel Nakitare, Full Stack Developer. This message will be overlaid on a large picture of the Sandia Mountains.</p>
 		<h2>About Me:</h2>
-		<p>This section with detail my technical skill set and professional competencies. I will highlight my strengths and personality. My core skills and programming languages will be prominently displayed in this section and the design will focus on making it easy to read quickly accessible from floating navigation bar via smooth scroll.</p>
+		<p>This section with detail my technical skill set and professional competencies. I will highlight my strengths and personality. My core skills and programming languages will be prominently displayed in this section and the design will focus on making it easy to read and quickly accessible from floating navigation bar via smooth scroll.</p>
 		<h2>Portfolio:</h2>
 		<p>I want this to be a responsive carousel containing 3-5 examples of my best work. Pop hovers will supply more information about my contribution to each project and the technology used.</p>
 		<h2>Contact:</h2>
-		<p>This section will contain a simple contact form and my professional email address. My professional social media links will live in the floating navigation bar.</p>
+		<p>This section will contain a simple contact form and my professional email address. My professional social media links will be easily accessible directly below the contact form.</p>
 		<h2>Wireframe:</h2>
 		<img src="PWP-Wireframe.png" alt="Wireframe">
 	</body>
