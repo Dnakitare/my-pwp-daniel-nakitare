276d06d976bda8471d0d1d9813495249ae110f8b
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-28 18:56:07 -0600

    milestone-2 wireframe mobile

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,15 +6,13 @@
 	</head>
 	<body>
 		<h2>Welcome:</h2>
-		<p>I am Daniel Nakitare and I am a Full Stack Developer. This message will be overlaid on a large picture of the Sandia Mountains.</p>
+		<p>Daniel Nakitare, Full Stack Developer. This message will be overlaid on a large picture of the Sandia Mountains.</p>
 		<h2>About Me:</h2>
 		<p>This section with detail my technical skill set and professional competencies. I will highlight my strengths and personality. My core skills and programming languages will be prominently displayed in this section and the design will focus on making it easy to read quickly accessible from floating navigation bar via smooth scroll.</p>
 		<h2>Portfolio:</h2>
 		<p>I want this to be a responsive carousel containing 3-5 examples of my best work. Pop hovers will supply more information about my contribution to each project and the technology used.</p>
 		<h2>Contact:</h2>
 		<p>This section will contain a simple contact form and my professional email address. My professional social media links will live in the floating navigation bar.</p>
-		<h2>Wireframes:</h2>
-		<p><strong>Mobile Version</strong></p>
-		<img src="PWP-wireframe-mobile.png" alt="Mobile Wireframe">
+		<h2>Wireframe:</h2>
 	</body>
 </html>
