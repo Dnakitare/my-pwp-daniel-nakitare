3aeaea3b5195ee90605899ec5142951301e5273b
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-28 10:09:57 -0700

    building about me section: adding more styling to icons and about me

--- public_html/scroll.js
+++ public_html/scroll.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 	$(window).scroll(function(){
 		$(".top").css("opacity", 1 - $(window).scrollTop() / 500);
 	});
+	$(window).scroll(function() {
+		$(".AbMeFade").css("opacity",-1 + $(window).scrollTop() / 400);
+	});
+	$(window).scroll(function() {
+		$(".iconFade").css("opacity",-1.5 + $(window).scrollTop() / 400);
+	});
 });
