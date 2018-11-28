33312629340e6be7bda8e31c019c44b8abd138b0
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-28 09:25:37 -0700

    added animate css, scroll magic, scroll.js, fonts

--- public_html/index.php
+++ public_html/index.php
@@ -24,6 +24,16 @@
 		<!-- Popper Bootstrap -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<!-- animate css -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+		<!-- scroll magic -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js"></script>
+		<!-- scroll.js -->
+		<script type="text/javascript" src="scroll.js"></script>
+		<!-- fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
 	</head>
 	<body>
 
